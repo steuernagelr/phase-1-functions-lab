1e56d5772d058b775fced069ea107d144a34010f
Rielly Steuernagel <reiguy@comcast.net>
2021-07-20 10:03:27 -0400

    commit tree 0de8508b5f8a02b473050f2e50cea745bc9fd324

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet(blocks) {
+    return Math.abs(blocks*264 - 42*264);
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(start - end)* 264
+}
+
+function calculatesFarePrice(start, end) {
+    if (Math.abs(start - end < 400))
+        return 0
+    else if (Math.abs(400 < start - end < 2000))
+        return .02 * start
+    else if (Math.abs(start - end > 2500))
+        return "cannot travel that far"
+}
